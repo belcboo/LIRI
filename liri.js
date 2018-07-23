f00077808c68985fa9ffcd2c03a14409e71b60f9
Francisco Garcia <belcboo@gmail.com>
2018-07-23 14:31:39 -0400

    OMDB working

---
+++ liri.js
@@ -0,0 +1,87 @@
+//Required packages.
+require("dotenv").config();
+let keys = require("./keys.js");
+var request = require("request");
+var Twitter = require("twitter");
+var Spotify = require("node-spotify-api");
+var fs = require("fs");
+
+//Declaring global variables.
+
+var operation = process.argv[2];
+var option1 = process.argv[3];
+var storeData = "";
+var textFile = "log.txt";
+var logval = "";
+var date;
+
+
+let spotify = new Spotify(keys.spotify);
+let client = new Twitter(keys.twitter);
+
+//OMDB Functionallity.
+var omdbLogic = {
+  evaluator: function() { //Checks if user type or not a movie.
+    if (option1 === undefined) { //If not sets the option to "Mr Nobody" and then calls the pull function
+      option1 = "Mr. Nobody";
+      omdbLogic.pull();
+    } else { //If there's a movie typed by the user then only calls the pull function.
+      omdbLogic.pull();
+    }
+  },
+  pull: function() {
+    logval += "movie-this " + option1 + "\n"
+    request("http://www.omdbapi.com/?t=" + option1 + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
+
+      // If the request is successful (i.e. if the response status code is 200)
+      if (!error && response.statusCode === 200) {
+        storeData = JSON.parse(body);
+        omdbLogic.print();
+      }
+    });
+  },
+  print: function() {
+    console.log("Title: " + storeData.Title,
+      "\nYear: " + storeData.Year,
+      "\nIMDB: " + storeData.imdbRating,
+      "\nRating Roten Tomatoes: " + storeData.Ratings[1].Value,
+      "\nCountry: " + storeData.Country,
+      "\nLanguage: " + storeData.Language,
+      "\nActors: " + storeData.Actors,
+      "\nPlot: " + storeData.Plot);
+    omdbLogic.log();
+  },
+
+  log: function() {
+    // date = new Date();
+    // console.log(date);
+    fs.appendFile(textFile, logval, function(err) {});
+  },
+}
+
+var spotifyLogic = {
+  search: function() {
+    spotify.search({
+        type: 'track',
+        query: 'All the Small Thingconsole.log(s'
+      })
+      .then(function(response) {
+        // console.log(response);
+        console.log(response.tracks.items[0]);
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+  },
+
+};
+
+switch (operation) {
+  case 'movie-this':
+    console.log(logval);
+    omdbLogic.evaluator();
+    break;
+  case 'spotify':
+    spotifyLogic.search();
+    break;
+}
